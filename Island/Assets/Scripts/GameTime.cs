c0e3b4db83cd6319e60f50c06ee04a34bb7d56f4
nameless118 <maks.kokurin.02@mail.ru>
2023-04-25 01:24:35 +0300

    добавил смену времени суток, течение игрового времени

--- Island/Assets/Scripts/GameTime.cs
+++ Island/Assets/Scripts/GameTime.cs
@@ -1,33 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class GameTime : MonoBehaviour
 {
-    public float timeMultiplier = 1.0f;
-    [SerializeField] Gradient LightGradient;
-    [SerializeField] Gradient ambientLightGradient;
+    //[SerializeField] Gradient LightGradient;
+    //[SerializeField] Gradient ambientLightGradient;
 
+    [Tooltip("Длительность суток в секундах")]
     [SerializeField, Range(1, 3600)] float timeDayInSeconds = 60;
+    [Tooltip("Время дня")]
     [SerializeField, Range(0f, 1f)] float timeProgress;
+    
+    //[SerializeField] float RotationSpeed = 1f;
 
-    [SerializeField] Light light;
+    [SerializeField] AnimationCurve SunCurve;
+    [SerializeField] AnimationCurve MoonCurve;
+    [SerializeField] AnimationCurve SkyboxCurve;
+
+    [SerializeField] Material DaySkybox;
+    [SerializeField] Material NightSkybox;
+
+    [SerializeField] Light Sun;
+    [SerializeField] Light Moon;
+
+    float sunIntensity, moonIntensity;
 
     Vector3 defaultAngles;
+    float _elapsedTime = 0f;
 
     private void Start() {
-        defaultAngles = light.transform.localEulerAngles;
+        defaultAngles = Sun.transform.localEulerAngles;
+        //DaySkybox.SetFloat("_Exposure", 1f);
+
+        sunIntensity = Sun.intensity;
+        moonIntensity = Moon.intensity;
     }
     private void Update() {
-        if (Application.isPlaying)
-            timeProgress += Time.deltaTime / timeDayInSeconds;
-        light.transform.rotation = Quaternion.Euler(timeProgress * 360f, 180, 0);
+        //_elapsedTime += Time.deltaTime;
+        timeProgress += Time.deltaTime / timeDayInSeconds;
+        //Sun.transform.rotation = Quaternion.Euler(timeProgress * 360f, 180, 0);
 
-        if (timeProgress > 1f)
-            timeProgress = 0f;
+        //DaySkybox.SetFloat("_Rotation", _elapsedTime * RotationSpeed);
+        //DaySkybox.SetFloat("_Exposure", Mathf.Clamp(Mathf.Sin(_elapsedTime), 0.15f, 1f));
 
-        light.color = LightGradient.Evaluate(timeProgress);
-        RenderSettings.ambientLight = ambientLightGradient.Evaluate(timeProgress);
+        if (timeProgress > 1f) timeProgress = 0f;
+
+        RenderSettings.skybox.Lerp(NightSkybox, DaySkybox, SkyboxCurve.Evaluate(timeProgress));
+        RenderSettings.sun = SkyboxCurve.Evaluate(timeProgress) > 0.1f ? Sun : Moon;
+        DynamicGI.UpdateEnvironment();
+
+        Sun.transform.localRotation = Quaternion.Euler(timeProgress * 360f, 180, 0);
+        Moon.transform.localRotation = Quaternion.Euler(timeProgress * 360f + 180f, 180, 0);
+
+        Sun.intensity = sunIntensity * SunCurve.Evaluate(timeProgress);
+        Moon.intensity = moonIntensity * MoonCurve.Evaluate(timeProgress);
+
+        //Sun.color = LightGradient.Evaluate(timeProgress);
+        //RenderSettings.ambientLight = ambientLightGradient.Evaluate(timeProgress);
     }
 }
